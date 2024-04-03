7da5c925691c0cec65a38d77f4eacf2868f93988
sasmitasahoodevops <145780695+sasmitasahoodevops@users.noreply.github.com>
2024-04-03 21:48:50 +0530

    Update main.tf

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -2,7 +2,7 @@ provider "aws"{
    region ="us-west-2"
    }
 resource "aws_instance" "example" {
-  ami           = "ami-0d47315cbfca67d83"
+  ami           = "ami-0eb199b995e2bc4e3"
   instance_type = "t2.micro"
   tags ={
       Name = "var.ec2_name"
