56b4b29c48d5870ec98c065d7b6dc3a7f3be2e03
sasmitasahoodevops <145780695+sasmitasahoodevops@users.noreply.github.com>
2024-04-03 21:37:36 +0530

    Update main.tf

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -1,10 +1,10 @@
 provider "aws"{
-   region ="us-east-2"
+   region ="us-west-2"
    }
-resource "aws_instance" "example"{
-  ami ="ami-0d47315cbfca67d83"
-  instance_type ="t2.micro"
+resource "aws_instance" "example" {
+  ami           = "ami-0d47315cbfca67d83"
+  instance_type = "t2.micro"
   tags ={
-      Name ="var.ec2_name"
+      Name = "var.ec2_name"
       }
-    }
+ }
